e9411a94a278ab93fbd4784ff6f7cad215350875
chasespengler <chasespengler7@gmail.com>
2022-06-14 15:15:24 -0400

    hero-section added

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -18,21 +18,21 @@ const Sidebar = ({isOpen, toggle}) => {
         </Icon>
         <SidebarWrapper>
             <SidebarMenu>
-                <SidebarLink to="about">
+                <SidebarLink to="about" onClick={toggle}>
                     About
                 </SidebarLink>
-                <SidebarLink to="discover">
+                <SidebarLink to="discover" onClick={toggle}>
                     Discover
                 </SidebarLink>
-                <SidebarLink to="services">
+                <SidebarLink to="services" onClick={toggle}>
                     Services
                 </SidebarLink>
-                <SidebarLink to="signup">
+                <SidebarLink to="signup" onClick={toggle}>
                     Sign Up
                 </SidebarLink>
             </SidebarMenu>
             <SideBtnWrap>
-                <SidebarRoute to="/signin">
+                <SidebarRoute to="/signin" onClick={toggle}>
                     Sign In
                 </SidebarRoute>
             </SideBtnWrap>
