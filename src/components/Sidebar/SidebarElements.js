9b7d68ee559283d3af16b667333be128cac7c699
chasespengler <chasespengler7@gmail.com>
2022-07-19 13:04:12 -0400

    info section

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -43,8 +43,8 @@ export const SidebarMenu  = styled.ul`
     grid-template-rows: repeat(6, 80px);
     text-align: center;
 
-    @media screena and (max-width: 480px) {
-        grid-template-rows: repeat(6, 60px);
+    @media screen and (max-width: 480px) {
+        grid-template-rows: repeat(5, 60px);
     }
 `
 
